1a93cab670679725815e39d74985cc501fe2557d
TonySergio <slayerid.montano@gmail.com>
2018-09-11 16:23:42 +0300

    start electron ok

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,21 @@
 const path = require('path');
 
 module.exports = {
+  presets: [['env', { modules: false }], 'es2015', 'react', 'stage-2'],
+
+  env: {
+    test: {
+      presets: ['env', 'react', 'stage-2'],
+    },
+  },
+
   plugins: [
+    'babel-plugin-add-module-exports',
+    'lodash',
+    'syntax-dynamic-import',
+    'transform-runtime',
+    'transform-decorators-legacy',
+
     [
       'module-resolver',
       {
